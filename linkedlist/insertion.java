dab851a903314390a39b6c85953365979dfb4794
deepanshuggupta <deepanshu.ggupta@gmail.com>
2017-12-31 18:39:47 +0530

    daily updatye

---
+++ linkedlist/insertion.java
@@ -0,0 +1,43 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		LinkedList lst = new LinkedList();
+		lst.push(1);
+		lst.push(2);
+		lst.push(3);
+		lst.display();
+	}
+}
+class LinkedList{
+	Node head =null;
+	void push(int data){
+		Node temp = new Node(data);
+		temp.next = head;
+		head = temp;
+	}
+	
+	void display(){
+		Node temp = head;
+		while(temp != null){
+			System.out.println("value is: " + temp.data);
+			temp = temp.next;
+		}
+	}
+} 
+class Node{
+	int data;
+	Node next;
+	Node(int d){
+		data= d;
+		next=null;
+	}
+}
